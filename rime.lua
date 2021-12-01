02c41f207f8bb77780e938f6d8ab653100c8348e
hellodk34 <huadekai@live.com>
2021-12-01 09:58:30 +0800

    update my rime config

--- rime.lua
+++ rime.lua
@@ -13,6 +13,7 @@ function date_translator(input, seg)
     if (input == "date") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d daily activity"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
